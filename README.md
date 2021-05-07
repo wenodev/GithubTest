5155e8ebc4faf4d887b67fd006de7fe369fc37e4
weno <logicalstark1105@gmail.com>
2021-05-07 17:49:42 +0900

    feat: 프로젝트 생성하라

---
+++ README.md
@@ -0,0 +1,4 @@
+# Spring Boot with Docker
+
+## 공식문서를 보고 테스트합니다.
+[공식문서](https://spring.io/guides/gs/spring-boot-docker/)
---
+++ src/test/java/com/example/bootDocker/controller/HelloControllerTest.java
@@ -0,0 +1,23 @@
+package com.example.bootDocker.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(HelloController.class)
+class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testGetHello() throws Exception {
+        mockMvc.perform(get("/hello")
+                .content("Hello"))
+                .andExpect(status().isOk());
+    }
+}
