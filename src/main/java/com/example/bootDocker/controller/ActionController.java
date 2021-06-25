6ce19b8f70e378ddf1857579284ee67133d68857
weno <logicalstark1105@gmail.com>
2021-06-25 10:13:30 +0900

    feat: action 컨트롤러 생성하라

---
+++ src/main/java/com/example/bootDocker/controller/ActionController.java
@@ -0,0 +1,14 @@
+package com.example.bootDocker.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ActionController {
+
+    @GetMapping("/action")
+    public String helloAction(){
+        return "Hello Action!";
+    }
+    
+}
